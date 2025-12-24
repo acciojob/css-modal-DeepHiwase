a529415ba5560af742cab01452782e03216acf7b
Deep Hiwase <114879645+DeepHiwase@users.noreply.github.com>
2025-12-24 11:42:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 //your JS code here. If required.
-	
+	document.getElementById('openModal').addEventListener('click', () => {
+  document.getElementById('modal').classList.remove('hidden');
+});
+
+document.querySelector('.close-modal').addEventListener('click', () => {
+  document.getElementById('modal').classList.add('hidden');
+});
+
+document.getElementById('modal').addEventListener('click', (e) => {
+  if (e.target.id === 'modal') {
+    document.getElementById('modal').classList.add('hidden');
+  }
+});
